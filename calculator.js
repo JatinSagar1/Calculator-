08895fb1da41c54c9ddcc8df6b478daf64ff7835
JatinSagar1 <150676293+JatinSagar1@users.noreply.github.com>
2025-04-30 23:16:26 +0530

    func

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("sr");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.innerText;
+
+        if (value === "AC") {
+            input.value = "";
+        } else if (value === "Back") {
+            input.value = input.value.slice(0, -1);
+        } else if (value === "=") {
+            try {
+                input.value = eval(input.value);
+            } catch {
+                input.value = "Error";
+            }
+        } else {
+            input.value += value;
+        }
+    });
+});
